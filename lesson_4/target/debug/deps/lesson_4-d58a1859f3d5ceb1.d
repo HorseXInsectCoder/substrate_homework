1d11b664c787c125c71ed41ed4454f38793d9ede
HorseXInsectCoder <cyhzero@gmail.com>
2022-04-15 10:58:54 +0800

    add lesson_4

---
+++ lesson_4/target/debug/deps/lesson_4-d58a1859f3d5ceb1.d
@@ -0,0 +1,5 @@
+D:\substrate\substrate_homework\lesson_4\target\debug\deps\lesson_4-d58a1859f3d5ceb1.exe: src\main.rs
+
+D:\substrate\substrate_homework\lesson_4\target\debug\deps\lesson_4-d58a1859f3d5ceb1.d: src\main.rs
+
+src\main.rs:
---
+++ lesson_4/target/debug/lesson_4.d
@@ -0,0 +1,1 @@
+D:\substrate\substrate_homework\lesson_4\target\debug\lesson_4.exe: D:\substrate\substrate_homework\lesson_4\src\main.rs
